2530ac3fed4a9910d57fbbc21271d1dade1751ae
MichaelCPP4 <michaelplay3452@gmail.com>
2022-12-12 14:31:20 +0300

    Добавьте файлы проекта.

---
+++ Lib/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    static public class Class
+    {
+        static public void SundP(int a, out int s, out int p)
+        {
+            s = a*a;
+            p = a*4;
+        }
+
+        static public int SwitchNumber(int number)
+        {
+            //string s = Convert.ToString(number % 10) + Convert.ToString(number / 10);
+            return int.Parse(Convert.ToString(number % 10) + Convert.ToString(number / 10));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib;
+
+namespace WPF_PR9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonSundP_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (int.TryParse(textBoxA.Text, out int a))
+            {
+                Class.SundP(a, out int s, out int p);
+                textBoxResult.Text = $"S= {s}, P= {p}";
+            }
+        }
+
+        private void ButtonSwitchNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textBoxNumber.Text, out int number))
+            {
+                //int number2 = number % 10;
+                textBoxResult2.Text = Class.SwitchNumber(number).ToString();
+                //textBoxResult2.Text = Convert.ToString(number%10) + Convert.ToString(number / 10);
+            }
+        }
+    }
+}
